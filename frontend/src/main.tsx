2ffeb4b17632a3704da7a13bf2d374778dba8bb8
Egor Starostenko <egorstarostenko345@gmail.com>
2026-02-25 14:43:27 +0200

    add tippy dependency

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./app";
 import { BrowserRouter } from "react-router-dom";
+import "tippy.js/dist/tippy.css";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
