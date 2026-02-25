c7c459349e8577f7f0b21a90547c73df24fd36b2
Egor Starostenko <egorstarostenko345@gmail.com>
2026-02-25 13:07:29 +0200

    app: add routing to HomePage

--- frontend/src/app/routing/Router.tsx
+++ frontend/src/app/routing/Router.tsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-import App from "../App";
+import HomePage from "../../pages/home/ui/HomePage";
 
 export default function Router() {
   return (
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<HomePage />} />
     </Routes>
   );
 }
