d2b1df45ba00dabce6fbe232204ba3b71fb9376e
Egor Starostenko <egorstarostenko345@gmail.com>
2026-02-27 12:56:00 +0200

    setting BookRows showing

--- frontend/src/pages/home/ui/HomePage.tsx
+++ frontend/src/pages/home/ui/HomePage.tsx
@@ -1,16 +1,15 @@
-import Detective from "../../../widgets/book-rows/ui/Detective";
-import Fantastic from "../../../widgets/book-rows/ui/Fantastic";
-import Horror from "../../../widgets/book-rows/ui/Horror";
-import HeroSlider from "../../../widgets/hero-slider/HeroSlider";
+import BookRow from "../../../widgets/book-row";
+import HeroSlider from "../../../widgets/hero-slider";
+
 import styles from "./HomePage.module.css";
 
 function HomePage() {
   return (
     <div className={styles.content}>
       <HeroSlider />
-      <Fantastic />
-      <Horror />
-      <Detective />
+      <BookRow genre="Детектив" />
+      <BookRow genre="Горор" />
+      <BookRow genre="Фантастика" />
     </div>
   );
 }
