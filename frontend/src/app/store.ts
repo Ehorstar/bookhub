0fd6b3f15e5341cd99e5d49bc1ec0747286768d0
Egor Starostenko <egorstarostenko345@gmail.com>
2026-02-27 12:52:22 +0200

    add cartApi to store

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { booksApi } from "../entities/Book/api/api";
+import { cartApi } from "../features/cart/api/cart.api";
 
 export const store = configureStore({
   reducer: {
     [booksApi.reducerPath]: booksApi.reducer,
+    [cartApi.reducerPath]: cartApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(booksApi.middleware),
+    getDefaultMiddleware().concat(booksApi.middleware, cartApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
