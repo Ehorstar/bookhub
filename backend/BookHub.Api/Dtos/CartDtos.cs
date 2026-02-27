9b751938327c84ab3191f95e17f402151b023656
Egor Starostenko <egorstarostenko345@gmail.com>
2026-02-27 18:45:16 +0200

    update cart and books logic, add Dtos

---
+++ backend/BookHub.Api/Dtos/CartDtos.cs
@@ -0,0 +1,18 @@
+﻿using BookHub.Api.Entities;
+
+namespace BookHub.Api.Dtos
+{
+    public class CartItemDto
+    {
+        public Book Book { get; set; } = null!;
+        public int Quantity { get; set; }
+        public decimal Total { get; set; } 
+    }
+
+    public class CartDto
+    {
+        public string CartId { get; set; } = null!;
+        public List<CartItemDto> Items { get; set; } = new();
+        public decimal Total { get; set; }
+    }
+}
