af6adf1e2bec381fef8cbde7cd398fdba313707f
Egor Starostenko <egorstarostenko345@gmail.com>
2026-02-27 22:33:07 +0200

    change currency to ETH

--- frontend/src/widgets/header/ui/Delivery.tsx
+++ frontend/src/widgets/header/ui/Delivery.tsx
@@ -10,7 +10,7 @@ const Delivery = () => {
 
       <div className={styles.text}>
         <p className={styles.title}>Безкоштовна доставка</p>
-        <p className={styles.subtitle}>від 990грн</p>
+        <p className={styles.subtitle}>від 990ETH</p>
       </div>
     </div>
   );
