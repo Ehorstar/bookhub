7b5e40bb6fb46394c39cc4838dd1b3d31e18640a
Egor Starostenko <egorstarostenko345@gmail.com>
2026-02-27 13:15:47 +0200

    make base of cart modal in header

---
+++ frontend/src/widgets/cart/ui/CartTotalBar.tsx
@@ -0,0 +1,22 @@
+import Arrow from "../../../shared/ui/Arrow/Arrow";
+import styles from "./CartTotalBar.module.css";
+
+type ToolbarProps = { total: number };
+
+function CartTotalBar({ total }: ToolbarProps) {
+  return (
+    <div className={`${styles.totalRow} ${styles.all}`}>
+      <div className={styles.totalLeft}>
+        <span className={styles.text}>Разом:</span>
+        <p className={styles.price}>{total}</p>
+        <span className={styles.text}>грн</span>
+      </div>
+
+      <div className={styles.totalRight}>
+        <Arrow text="Продовжити" className={styles.Arrow} />
+      </div>
+    </div>
+  );
+}
+
+export default CartTotalBar;
