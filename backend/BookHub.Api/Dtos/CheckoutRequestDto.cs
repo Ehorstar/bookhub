3cee9cfdeabc34453ca657b59c0e5069b6569867
Egor Starostenko <egorstarostenko345@gmail.com>
2026-03-02 23:21:57 +0200

    add checkout request dto

---
+++ backend/BookHub.Api/Dtos/CheckoutRequestDto.cs
@@ -0,0 +1,10 @@
+﻿public class CheckoutRequestDto
+{
+    public string City { get; set; } = "";
+    public string Department { get; set; } = "";
+    public string Phone { get; set; } = "";
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
+    public string TxHash { get; set; } = "";
+
+}
